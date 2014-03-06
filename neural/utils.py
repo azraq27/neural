9df3a7c82eced775bb7838d653152610e3b0bd98
Bill Gross <bill.gross@me.com>
2014-03-06 14:58:45 -0600

    fixed factor bug

--- neural/utils.py
+++ neural/utils.py
@@ -112,4 +112,4 @@ class simple_timer:
 
 def factor(n):
 	'''return set of all prime factors for a number'''
-    return set(reduce(list.__add__, ([i, n//i] for i in range(1, int(n**0.5) + 1) if n % i == 0)))
+	return set(reduce(list.__add__, ([i, n//i] for i in range(1, int(n**0.5) + 1) if n % i == 0)))
