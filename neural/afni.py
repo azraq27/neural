a76bdb523d0b912adfd9e73cbaf637af4bb15f63
Bill Gross <bill.gross@me.com>
2014-03-27 15:21:37 -0500

    updated qwarp_invert

--- neural/afni.py
+++ neural/afni.py
@@ -522,9 +522,12 @@ def qwarp_invert(warp_param_dset,output_dset,affine_1Dfile=None):
     '''inverts a qwarp (defined in ``warp_param_dset``) (and concatenates affine matrix ``affine_1Dfile`` if given)
     outputs the inverted warp + affine to ``output_dset``'''
     
-    cmd = ['3dNwarpCat','-prefix',output_dset,
-           '-warp1','INV(%s)' % warp_param_dset]
+    cmd = ['3dNwarpCat','-prefix',output_dset]
+    
     if affine_1Dfile:
-        cmd += ['-warp2','INV(%s)' % affine_1Dfile]
+        cmd += ['-warp2','INV(%s)' % warp_param_dset
+        '-warp1','INV(%s)' % affine_1Dfile]
+    else:
+        cmd += ['-warp1','INV(%s)' % warp_param_dset]
     neural.run(cmd,products=output_dset)
 
