d616c8a81fa6413df7913728ae477b47a9a88ce2
Bill Gross <bill.gross@me.com>
2014-03-31 13:42:20 -0500

    silly bug

--- neural/__init__.py
+++ neural/__init__.py
@@ -3,14 +3,15 @@
 Library to provide helper functions to make analyzing
 neuroimaging data in python a little easier'''
 
+version = 0.2
+
+#! flag to indicate whether most functions should be verbose or not
+verbose = True
+
 from utils import *
 
 import afni
 import eprime
 import dicom
 
-version = 0.2
-
-#! flag to indicate whether most functions should be verbose or not
-verbose = True
 
