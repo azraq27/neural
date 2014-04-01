b38a32dbe1e3f6cda07cac53d527786ad3bf8608
Bill Gross <bill.gross@me.com>
2014-04-01 17:37:41 -0500

    fsl is weird

--- neural/fsl.py
+++ neural/fsl.py
@@ -9,5 +9,5 @@ def skull_strip(dset,suffix='_ns'):
     # should add options to use betsurf and T1/T2 in the future
     out_dset = nl.afni.suffix(dset,suffix)
     cmd = bet2 if bet2 else 'bet2'
-    cmd = os.path.join(fsl_dir,cmd) if fsl_dir
+    cmd = os.path.join(fsl_dir,cmd) if fsl_dir else cmd
     nl.run([cmd,dset,out_dset],products=out_dset)
