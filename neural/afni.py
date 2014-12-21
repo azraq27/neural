cfc17ca7861815cd56c12462f70765ceacc50cca
Bill Gross <bill.gross@me.com>
2014-12-20 19:39:13 -0600

    bug in create_censor_file

--- neural/afni.py
+++ neural/afni.py
@@ -756,7 +756,7 @@ def create_censor_file(input_dset,out_prefix=None,fraction=0.1,clip_to=0.1,max_e
     info = dset_info(input_dset)
     outcount = [float(x) for x in subprocess.check_output(['3dToutcount','-fraction','-automask','-polort',str(polort),str(input_dset)]).split('\n') if len(x.strip())>0]
     binary_outcount = [x<fraction for x in outcount]
-    perc_outliers = sum(binary_outcount)/float(info.reps)
+    perc_outliers = 1 - (sum(binary_outcount)/float(info.reps))
     if max_exclude and perc_outliers > max_exclude:
         raise AFNI_Censor_TooManyOutliers('Found %f outliers in dset %s' % (perc_outliers,input_dset))
     if clip_to:
