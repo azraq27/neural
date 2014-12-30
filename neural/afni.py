3638d19ebb701808843fd0d9b893b52d34ea8eea
Bill Gross <bill.gross@me.com>
2014-12-30 00:16:00 -0600

    bugfix

--- neural/afni.py
+++ neural/afni.py
@@ -861,12 +861,12 @@ def smooth_decon_to_fwhm(decon,fwhm):
         cwd = os.getcwd()
         random_files = [re.sub(r'\[\d+\]$','',x) for x in nl.flatten([x for x in decon.__dict__.values() if isinstance(x,basestring) or isinstance(x,list)]+[x.values() for x in decon.__dict__.values() if isinstance(x,dict)])]
         files_to_copy = [x for x in random_files if os.path.exists(x) and x[0]!='/']
+        # copy crap
+        for file in files_to_copy:
+            print 'copying %s' % file
+            shutil.copytree(file,tmpdir)
         with nl.run_in(tmpdir):
             decon.errts = 'residual.nii.gz'
-            # copy crap
-            for file in files_to_copy:
-                print 'copying %s' % file
-                shutil.copytree(file,tmpdir)
             decon.run()
             running_reps = 0
             blur_input = lambda i: 'input_blur-part%d.nii.gz'%(i+1)
