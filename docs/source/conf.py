53175bdc0066096d3f944d1a06513142542e348c
Bill Gross <bill.gross@me.com>
2015-04-19 16:33:47 -0500

    doc updates

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ import os
 
 sys.path.append(os.path.abspath('../../neural'))
 sys.path.append(os.path.abspath('../../neural/neural'))
-import neural
+import wrappers
 #import neural.wrappers.common
 
 # -- General configuration ------------------------------------------------
