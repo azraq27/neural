d87ede3ab37bcab6e4f3132ab702cd7ab41e44aa
Bill Gross <bill.gross@me.com>
2015-04-21 03:51:23 -0500

    bugfix release

--- neural/__init__.py
+++ neural/__init__.py
@@ -3,7 +3,7 @@
 Library to provide helper functions to make analyzing
 neuroimaging data in python a little easier'''
 
-version = '1.0'
+version = '1.0.1'
 
 #! flag to indicate whether most functions should be verbose or not
 verbose = True
--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from distutils.core import setup
 
-version = '1.0'
+version = '1.0.1'
 
 setup(
   name = 'neural-fmri',
