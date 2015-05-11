3f19359fe871ca73b35eb25b6191a31825495955
bgross <bill.gross@me.com>
2015-05-11 13:00:35 -0500

    changed around fsl/skullstrip to normalize the signal first

--- neural/wrappers/fsl.py
+++ neural/wrappers/fsl.py
@@ -15,7 +15,12 @@ def binary_available():
 def skull_strip(dset,suffix='_ns'):
     ''' use bet to strip skull from given anatomy '''
     # should add options to use betsurf and T1/T2 in the future
+    # Since BET fails on weirdly distributed datasets, I added 3dUnifize in... I realize this makes this dependent on AFNI. Sorry, :)
     out_dset = nl.suffix(dset,suffix)
+    unifize_dset = nl.suffix(dset,'_u')
     cmd = bet2 if bet2 else 'bet2'
+    info = nl.dset_info(dset)
     cmd = os.path.join(fsl_dir,cmd) if fsl_dir else cmd
-    nl.run([cmd,dset,out_dset],products=out_dset)
+    cutoff_value = info.subbricks[0]['max'] * 0.05
+    nl.run(['3dUnifize','-prefix',unifize_dset,nl.calc(dset,'step(a-%f)*a' % cutoff_value)],products=unifize_dset)
+    nl.run([cmd,unifize_dset,out_dset],products=out_dset)
