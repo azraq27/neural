7e7019e8889a26cfffbd0f7b4c684d44ff94e375
bgross <bill.gross@me.com>
2015-05-11 12:59:54 -0500

    added min/max to dset_info

--- neural/dsets.py
+++ neural/dsets.py
@@ -142,13 +142,15 @@ def _dset_info_afni(dset):
     if raw_info==None:
         return None
     # Subbrick info:
-    sub_info = re.findall(r'At sub-brick #(\d+) \'([^\']+)\' datum type is (\w+).*(\n.*statcode = (\w+);  statpar = (.*)|)',raw_info)
+    sub_info = re.findall(r'At sub-brick #(\d+) \'([^\']+)\' datum type is (\w+):\s+(\d+)\s+to\s+(\d+).*(\n.*statcode = (\w+);  statpar = (.*)|)',raw_info)
     for brick in sub_info:
         info.subbricks.append({
             'label': brick[1],
             'datum': brick[2],
-            'stat': brick[4],
-            'params': brick[5].split()
+            'min': float(brick[3]),
+            'max': float(brick[4]),
+            'stat': brick[6],
+            'params': brick[7].split()
         })
     info.reps = len(info.subbricks)
     # Dimensions:
