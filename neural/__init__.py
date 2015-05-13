f2ebb08e74109bc11b16343f05bd0feb533da6d9
Bill Gross <bill.gross@me.com>
2015-05-12 21:01:47 -0500

    release

--- neural/__init__.py
+++ neural/__init__.py
@@ -3,7 +3,7 @@
 Library to provide helper functions to make analyzing
 neuroimaging data in python a little easier'''
 
-version = '1.0.4'
+version = '1.0.5'
 
 #! flag to indicate whether most functions should be verbose or not
 verbose = True
