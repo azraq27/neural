4fb58a59137f4d39f9a6049481ad0fd6fd53e398
Bill Gross <bill.gross@me.com>
2015-06-02 10:55:06 -0500

    updated affine_align

--- neural/alignment.py
+++ neural/alignment.py
@@ -9,7 +9,7 @@ def volreg(dset,suffix='_volreg',base_subbrick=3,tshift=True,cost=None):
     cmd += [dset]
     nl.run(cmd,products=nl.suffix(dset,suffix))
 
-def affine_align(dset_from,dset_to,skull_strip=True,mask=None,affine_suffix='_aff'):
+def affine_align(dset_from,dset_to,skull_strip=True,mask=None,affine_suffix='_aff',opts=[]):
     ''' interface to 3dAllineate to align anatomies and EPIs '''
     
     dset_ss = lambda dset: os.path.split(nl.suffix(dset,'_ns'))[1]
@@ -47,7 +47,7 @@ def affine_align(dset_from,dset_to,skull_strip=True,mask=None,affine_suffix='_af
         '-1Dmatrix_save', dset_affine_1D,
         '-autoweight',
         '-cmass'
-    ]
+    ] + opts
     if cost:
         all_cmd += ['-cost',cost]    
     if mask:
