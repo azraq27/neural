12cbfd35fa3410e6c56fbeafdee5b2dec6aa3f60
Bill Gross <bill.gross@me.com>
2016-01-07 22:05:24 -0600

    added create_dset_to3d to manually create datasets that fail with autocreation

--- neural/dicom.py
+++ neural/dicom.py
@@ -303,6 +303,75 @@ def _create_dset_dicom(directory,slice_order='alt+z',sort_order=None):
                 return False
         finally:
             shutil.rmtree(sorted_dir)
+            
+def create_dset_to3d(prefix,file_list,file_order='zt',num_slices=None,num_reps=None,TR=None,slice_order='alt+z',only_dicoms=True):
+    '''manually create dataset by specifying everything (not recommended, but necessary when autocreation fails)
+    
+    if `num_slices` or `num_reps` is omitted, it will be inferred by the number of images
+    
+    `only_dicoms` will filter the given list by readable DICOM images'''
+    tags = {
+        'num_rows': (0x0028,0x0010),
+        'TR': (0x0018,0x0080)
+    }
+    with nl.notify('Trying to create dataset %s' % prefix):
+        if os.path.exists(prefix):
+            nl.notify('Error: file "%s" already exists!' % prefix,level=nl.level.error)
+            return False
+        
+        if only_dicoms:
+            new_file_list = []
+            for f in file_list:
+                try:
+                    if len(info_for_tags(f,tags['num_rows']))>0:
+                        # Only include DICOMs that actually have image information
+                        new_file_list.append(f)
+                except:
+                    pass
+            file_list = new_file_list
+        
+        if len(file_list)==0:
+            nl.notify('Error: Couldn\'t find any valid DICOM images',level=nl.level.error)
+            return False
+        
+        if num_slices==None and num_reps==None:
+            nl.notify('Error: create_dset_to3d will not try to guess both slices and reps! (use create_dset if you want that)',level=nl.level.error)
+            return False
+        if num_slices==None:
+            if len(file_list)%num_reps!=0:
+                nl.notify('Error: trying to guess # of slices, but %d (number for files) doesn\'t divide evenly into %d (number of reps)' % (len(file_list),num_reps),level=nl.level.error)
+                return False
+            num_slices = len(file_list)/num_reps
+        if num_reps==None:
+            if len(file_list)%num_slices!=0:
+                nl.notify('Error: trying to guess # of reps, but %d (number for files) doesn\'t divide evenly into %d (number of slices)' % (len(file_list),num_reps),level=nl.level.error)
+                return False
+            num_reps = len(file_list)/num_slices
+        
+        if TR==None:
+            i = info_for_tags(file_list[0],[tags['TR']])
+            TR = i[tags['TR']]
+
+        cmd = ['to3d','-skip_outliers','-quit_on_err','-prefix',prefix]
+        cmd += ['-time:%s'%file_order]
+        if file_order=='zt':
+            cmd += [num_slices,num_reps]
+        else:
+            cmd += [num_reps,num_slices]
+        cmd += [TR,slice_order]
+        cmd += ['-@']
+        cmd = [str(x) for x in cmd]
+        out = None
+        try:
+            p = subprocess.Popen(cmd,stdin=subprocess.PIPE,stdout=subprocess.PIPE,stderr=subprocess.PIPE)
+            out = p.communicate('\n'.join(file_list))
+            if p.returncode!=0:
+                raise Exception
+        except:
+            with nl.notify('Error: to3d returned error',level=nl.level.error):
+                if out:
+                    nl.notify('stdout:\n' + out[0] + '\nstderr:\n' + out[1],level=nl.level.error)
+            return False
 
 def create_dset(directory,slice_order='alt+z',sort_order='zt'):
     '''tries to autocreate a dataset from images in the given directory'''
