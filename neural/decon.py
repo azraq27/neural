4592805e28572d3256c8e95177f312dfd7ce5eab
Bill Gross <bill.gross@me.com>
2016-12-15 15:30:48 -0600

    nodata bug

--- neural/decon.py
+++ neural/decon.py
@@ -3,11 +3,11 @@ import neural as nl
 
 class Decon:
     '''wrapper for AFNI 3dDeconvolve command
-    
+
     Properties:
         :input_dset:        ``list`` of input datasets
         :decon_stims:       ``list`` of :class:`DeconStim` objects that define the different stimuli. Allows
-                            for flexible coding of complex stimuli. Alternatively, for simpler stimuli, you 
+                            for flexible coding of complex stimuli. Alternatively, for simpler stimuli, you
                             can simply use one of the traditional options below (e.g., ``stim_files`` or ``stim_times``)
         :stim_files:        ``dict`` where keys are used as stimulus labels
                             and the values are taken as 1D files
@@ -16,7 +16,7 @@ class Decon:
                             listed stimuli (optional)
         :model_default:     default model to use for each ``stim_times`` stimulus if nothing
                             is listed in ``models``
-        :stim_base:         ``list`` of names of stimuli (defined either in stim_files or 
+        :stim_base:         ``list`` of names of stimuli (defined either in stim_files or
                             stim_times) that should be considered in the baseline instead
                             of full model
         :stim_am1:          ``list`` of names of stimuli defined in stim_times that should
@@ -27,21 +27,21 @@ class Decon:
                             is a symbolic statement
         :mask:              either a mask file, or "auto", which will use "-automask"
         :errts:             name of file to save residual time series to
-        
+
         Options that are obvious:
             nfirst (default: 3), censor_file, polort (default: 'A'), tout, vout, rout, prefix
-                    
+
         Other options:
-        
+
         :opts:              list of extra things to put directly in the command (in case none of the above options cover you)
         :partial:           ``dict`` with keys of datasets (given in ``input_dsets``) and values of a ``tuple`` of (start,end) for each given
                             dataset. Will automatically adjust all stimfiles and such to match. **Column-style stimuli only work if there's only
                             one dataset**
-        
-        
-        
+
+
+
         **Example:**::
-        
+
             decon = neural.decon.Decon()
             decon.input_dsets = ['dset_run1.nii.gz', 'dset_run2.nii.gz']
             decon.censor_file = 'subject_censor.1D'
@@ -92,12 +92,12 @@ class Decon:
         self.stim_sds = None
         self.errts = None
         self.partial = {}
-        
+
         self._del_files = []
-    
+
     def command_list(self):
         '''returns the 3dDeconvolve command as a list
-    
+
         The list returned can be run by passing it into a subprocess-like command
         (e.g., neural.run())
         '''
@@ -125,7 +125,7 @@ class Decon:
                         self._del_files.append(f.name)
             cmd += ['-censor', censor_file]
         nfirst = self.nfirst
-        if self.input_dsets[0] in self.partial:
+        if len(self.input_dsets) and self.input_dsets[0] in self.partial:
             nfirst -= self.partial[self.input_dsets[0]][0]
         if nfirst<0:
             nfirst = 0
@@ -136,9 +136,9 @@ class Decon:
             else:
                 cmd += ['-mask',self.mask]
         cmd += ['-polort',str(self.polort)]
-                
+
         stim_num = 1
-        
+
         all_stims = list(self.decon_stims)
         all_stims += [DeconStim(stim,column_file=self.stim_files[stim],base=(stim in self.stim_base)) for stim in self.stim_files]
         for stim in self.stim_times:
@@ -148,7 +148,7 @@ class Decon:
             decon_stim.AM2 = (stim in self.stim_am2)
             decon_stim.base = (stim in self.stim_base)
             all_stims.append(decon_stim)
-        
+
         if self.partial:
             for i in xrange(len(self.input_dsets)):
                 if self.input_dsets[i] in self.partial:
@@ -158,9 +158,9 @@ class Decon:
                         if stim:
                             new_stims.append(stim)
                     all_stims = new_stims
-        
+
         cmd += ['-num_stimts',len(all_stims)]
-            
+
         for stim in all_stims:
             column_file = stim.column_file
             if stim.column!=None:
@@ -195,7 +195,7 @@ class Decon:
                 if stim.base:
                     cmd += ['-stim_base',stim_num]
                 stim_num += 1
-        
+
         strip_number = r'[-+]?(\d+)?\*?(.*)'
         all_glts = {}
         stim_names = [stim.name for stim in all_stims]
@@ -211,12 +211,12 @@ class Decon:
                 all_glts[glt] = self.glts[glt]
 
         cmd += ['-num_glt',len(all_glts)]
-        
+
         glt_num = 1
         for glt in all_glts:
             cmd += ['-gltsym','SYM: %s' % all_glts[glt],'-glt_label',glt_num,glt]
             glt_num += 1
-        
+
         if self.bout:
             cmd += ['-bout']
         if self.tout:
@@ -225,29 +225,29 @@ class Decon:
             cmd += ['-vout']
         if self.rout:
             cmd += ['-rout']
-        
+
         if self.errts:
             cmd += ['-errts', self.errts]
-        
+
         if self.prefix:
             cmd += ['-bucket', self.prefix]
-        
+
         return [str(x) for x in cmd]
-    
+
     def __del__(self):
         for f in self._del_files:
             try:
                 os.unlink(f)
             except:
                 pass
-    
+
     def command_string(self):
         '''returns the 3dDeconvolve command as as string
-        
+
         This command can then be run in something like a shell script
         '''
         return ' '.join(self.command_list())
-    
+
     def run(self):
         '''runs 3dDeconvolve through the neural.utils.run shortcut'''
         out = nl.run(self.command_list(),products=self.prefix)
@@ -260,20 +260,20 @@ class Decon:
 
 class DeconStim(object):
     '''encapsulates the definition of any arbitrary stimulus, along with all of its 3dDeconvolve options
-    
+
     Attributes:
         :name:          Label used for the stimulus
-        
+
         You only need to define one of the following to define the stimulus:
         :column:        A ``list`` of numbers (usually the length of the runs) that is placed directly in the 3dDeconvolve
                         matrix. This can be a series of 1's and 0's, a 1D-file that has already been convolved with an HRF,
                         or a covariate like motion parameters
-        :column_file:   The filename of a file that contains a matrix column 
+        :column_file:   The filename of a file that contains a matrix column
         :times:         Definition of stimuli using ``stim_times`` approach.  Either a ``list`` or a ``list`` of ``list``s
                         (one for each run) containing either ``float``s or strings of the stimulus times. For details on
                         how to define the stimulus time, see the documentation of ``3dDeconvolve``.
         :times_file:    A file containing a ``stim_times`` stimulus
-        
+
         :times_model:   If using ``stim_times``, which HRF model to use
         :base:          Is this stimulus part of the baseline? (``True``/``False``)
         :AM1:           Does this stimulus use an "AM1" model? (``True``/``False``)
@@ -281,7 +281,7 @@ class DeconStim(object):
 
         :reps:          Number of reps in associated fMRI run (helpful when manipulating the stims)
         :TR:            TR of the associated fMRI run
-    
+
     '''
     def __init__(self,name,column_file=None,times_file=None,model='GAM',base=False,AM1=False,AM2=False):
         self.name = name
@@ -290,7 +290,7 @@ class DeconStim(object):
         self.times = None
         self.times_file = times_file
         self.times_model = model
-        
+
         self.base = base
         self.AM1 = AM1
         self.AM2 = AM2
@@ -371,7 +371,7 @@ class DeconStim(object):
             concat_stim.times = self.times + decon_stim.times
             return concat_stim
         return None
-        
+
     def partial(self,start=0,end=None,run=0):
         '''chops the stimulus by only including time points ``start`` through ``end`` (in reps, inclusive; ``None``=until the end)
         if using stim_times-style simulus, will change the ``run``'th run. If a column, will just chop the column'''
@@ -379,11 +379,11 @@ class DeconStim(object):
         decon_stim = copy.copy(self)
         if start<0:
             start = 0
-        if self.type()=="column": 
+        if self.type()=="column":
             decon_stim.column_file = None
             if end>=len(decon_stim.column):
                 end = None
-            if end==None:                
+            if end==None:
                 decon_stim.column = decon_stim.column[start:]
             else:
                 decon_stim.column = decon_stim.column[start:end+1]
@@ -406,7 +406,7 @@ class DeconStim(object):
                 if a>=start and (end==None or a<=end):
                     return True
                 return False
-            
+
             decon_stim.times_file = None
             if len(decon_stim.times)==0 or '__iter__' not in dir(decon_stim.times[0]):
                 decon_stim.times = [decon_stim.times]
@@ -418,7 +418,7 @@ class DeconStim(object):
 def stack_decon_stims(stim_list):
     '''take a ``list`` (in order of runs) of ``dict``s of stim_name:DeconStim and stack them together. returns
     a single ``dict`` of stim_name:decon_stim
-    
+
     As in, takes:
     [
         # Run 1
@@ -426,10 +426,10 @@ def stack_decon_stims(stim_list):
         # Run 2
         { "stim1": decon_stim1b, "stim2": decon_stim2b, "stim3": decon_stim3 }
     ]
-    
+
     And makes:
         { "stim1": decon_stim1, "stim2": decon_stim2, "stim3": decon_stim3 }
-        
+
     If a stimulus is not present in a run, it will fill that run with an empty stimulus
     '''
     stim_names = list(set(nl.flatten([stims.keys() for stims in stim_list])))
@@ -441,7 +441,7 @@ def stack_decon_stims(stim_list):
             nl.notify('Error: Trying to stack stimuli of different types! (%s)' % stim_name,level=nl.level.error)
             return None
         type = types[0]
-        
+
         stim_stack = []
         for i in xrange(len(stim_list)):
             if stim_name in stim_list[i]:
@@ -459,7 +459,7 @@ def smooth_decon_to_fwhm(decon,fwhm,cache=True):
     if os.path.exists(decon.prefix):
         return
     blur_dset = lambda dset: nl.suffix(dset,'_smooth_to_%.2f' % fwhm)
-    
+
     with nl.notify('Running smooth_decon_to_fwhm analysis (with %.2fmm blur)' % fwhm):
         tmpdir = tempfile.mkdtemp()
         try:
@@ -474,12 +474,12 @@ def smooth_decon_to_fwhm(decon,fwhm,cache=True):
                 except OSError as e:
                     shutil.copy(file,tmpdir)
                 shutil.copy(file,tmpdir)
-            
+
             copyback_files = [decon.prefix,decon.errts]
             with nl.run_in(tmpdir):
                 if os.path.exists(decon.prefix):
                     os.remove(decon.prefix)
-                
+
                 # Create the blurred inputs (or load from cache)
                 if cache and all([os.path.exists(os.path.join(cwd,blur_dset(dset))) for dset in decon.input_dsets]):
                     # Everything is already cached...
